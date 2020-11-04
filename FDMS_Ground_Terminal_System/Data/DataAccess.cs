00b45900561fecb8635c231141d888f38cc3fc8b
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-11-04 10:30:52 -0500

    removed useless material from template

---
+++ FDMS_Ground_Terminal_System/Data/DataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FDMS_Ground_Terminal_System.Data
+{
+    public class DataAccess
+    {
+    }
+}
