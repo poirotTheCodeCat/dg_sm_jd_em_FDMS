e5c47c56fc1c246742c879eb9f16e426369ce6a9
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-11-04 17:44:41 -0500

    added private data members to gforce class

--- FDMS/GForce.cs
+++ FDMS/GForce.cs
@@ -6,5 +6,9 @@ namespace FDMS
 {
     class GForce
     {
+        private float accel_x { get; set; }
+        private float accel_y { get; set; }
+        private float accel_z { get; set; }
+        private float weight { get; set; }
     }
 }
