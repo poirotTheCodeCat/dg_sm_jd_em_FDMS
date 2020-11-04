fcad0fdcf19856f2a4ad0fe32ab3d5bae1ff3d58
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-11-04 17:38:44 -0500

    added Telemetry related classes

--- FDMS/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FDMS
-{
-    public class Class1
-    {
-    }
-}
---
+++ FDMS/Telemetry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FDMS
+{
+    public class Telemetry
+    {
+        String tailNum { get; set; }
+        GForce GforceParameters { get; set; }
+        Altitude AltitudeParameters { get; set; }
+        DateTime timeStamp { get; set; }
+    }
+}
