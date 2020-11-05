28a86c9363ac3da55e0f11f169e08dfb398fd6b1
Jimmy-Du <jdu5780@conestogac.on.ca>
2020-11-05 17:15:40 -0500

    Added parsing functionality for aircraft files.

--- FDMS_Aircraft_Transmission_System/Program.cs
+++ FDMS_Aircraft_Transmission_System/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Sockets;
 
 namespace FDMS_Aircraft_Transmission_System
 {
@@ -6,7 +8,44 @@ namespace FDMS_Aircraft_Transmission_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string serverName = "127.0.0.1";
+            int port = 13000;
+
+            TcpClient client = new TcpClient(serverName, port);
+            NetworkStream stream = client.GetStream();
+
+            sendPackets("C:\\tmp\\C-FGAX.txt", client, stream);
+            sendPackets("C:\\tmp\\C-GEFC.txt", client, stream);
+            sendPackets("C:\\tmp\\C-QWWT.txt", client, stream);
+
+            client.Close();
+            stream.Close();
         }
+
+
+
+        // Method:      sendPackets()
+        // Description: 
+        // Parameters:  string filename: the filename of the file that will have its contents read and sent to via packets
+        //              TcpClient client:
+        //              NetworkStream stream:
+        // Returns:     N/A
+        static void sendPackets(string filename, TcpClient client, NetworkStream stream)
+        {
+            string[] aircraftData = null;
+
+            string[] lines = System.IO.File.ReadAllLines(filename);
+
+            foreach (string line in lines)
+            {
+                if (!line.Equals(lines.Last()))
+                {
+                    aircraftData = line.Split(',');
+                }
+            }
+        }
+
+
+
     }
 }
