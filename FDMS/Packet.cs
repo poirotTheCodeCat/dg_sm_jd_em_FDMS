e56161279f1812deb920364661685e9043df7593
Jimmy-Du <56742902+Jimmy-Du@users.noreply.github.com>
2020-11-06 01:00:32 -0500

    Updated the packet class

--- FDMS/Packet.cs
+++ FDMS/Packet.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +6,47 @@ namespace FDMS
 {
     public class Packet
     {
+        private Telemetry telemetryData;
+        private int packetSequence;
+        private int checksum;
 
+        public Packet()
+        {
+
+        }
+
+        public Packet(Telemetry initalTelemetry, int initalSequence)
+        {
+            telemetryData = initalTelemetry;
+            packetSequence = initalSequence;
+            checksum = calculateCheckSum();
+        }
+
+        public int CheckSum
+        {
+            get { return checksum; }
+            set { checksum = value; }
+        }
+
+        public int PacketSequence
+        {
+            get { return packetSequence; }
+            set { packetSequence = value; }
+        }
+
+        public Telemetry TelemetryData
+        {
+            get { return telemetryData; }
+            set { telemetryData = value; }
+        }
+
+        public int calculateCheckSum()
+        {
+            double result = 0;
+
+            result = (telemetryData.Altitude + telemetryData.Pitch + telemetryData.Bank) / 3;
+
+            return (int)result;
+        }
     }
 }
