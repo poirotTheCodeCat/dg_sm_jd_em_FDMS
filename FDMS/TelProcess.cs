630b233499417089db37d6282a4d6d7e65980c35
Daniel Grew <Dgrew0287@conestogac.on.ca>
2020-11-06 12:43:31 -0500

    Added TelProcess class

---
+++ FDMS/TelProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FDMS
+{
+    public static class TelProcess
+    {
+        public static Telemetry process(byte[] packetBytes)
+        {
+            Packet packet = JsonSerializer.Deserialize<Packet>(packetBytes);
+            return process(packet);
+        }
+
+        public static Telemetry process(Packet packet)
+        {
+            // check the checksum
+
+            return null;
+        }
+    }
+}
