a22eb36a6e8a61c3dc22c8d13cd25bc1a35a98b8
Sasha Malesevic <smalesevic7531@conestogac.on.ca>
2020-11-05 22:59:06 -0500

    Working DB connection and insert stored procedure

---
+++ FDMS/ISqlDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FDMS
+{
+    public interface ISqlDataAccess
+    {
+        string ConnectionString { get; set; }
+
+        Task<List<T>> LoadData<T, U>(string sql, U parameters);
+        Task SaveData<T>(string sql, T parameters);
+    }
+}
---
+++ FDMS/SqlDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Linq;
+
+namespace FDMS
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionString { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionString);
+            //string connectionString = "Data Source = SASHAMALESEFDB0; Initial Catalog = GroundTerminal; Integrated Security = True";
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionString);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+
+            }
+        }
+    }
+}
